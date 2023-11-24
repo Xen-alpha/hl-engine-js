77c5fb84bb6cb4b0e6d41285388d2ea86c07c3cb
Xen-alpha <hky7548@naver.com>
2023-11-24 15:37:14 +0900

    final edit for libertygame wiki

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 import {init, start, fullscreen} from 'https://cdn.jsdelivr.net/gh/Xen-alpha/hl-engine-js@main/lib/hl-engine.js';
 
-var xashgamename = "valve"
-var xashfullscreen = false;
+var uri = new mw.Uri(document.URL);
+var xashgamename = uri.query["gamename"];
+// var xashfullscreen = false;
 
 document.getElementById('start').onclick = () => {
   const reader = new FileReader();
   reader.onload = function(){
     const params = {
-      mod: document.getElementById('mod').value,
-      map: document.getElementById('map').value,
-      filesystem: "RAM",
-      fullscreen: xashfullscreen,
+      mod: xashgamename,
+      map: "",
+      filesystem: "LocalStorage",
+      fullscreen: false,
       zip: reader.result,
       args: [
         "+volume",
         "0.5",
       ],
     };
+    document.getElementById("start").style.display = "none";
     start(params);
   }
   reader.readAsArrayBuffer(document.getElementById('zip').files[0]);
 }
 
-document.getElementById('xash-fullscreen').onclick = fullscreen;
+// document.getElementById('xash-fullscreen').onclick = fullscreen;
 
 function setStatus(text) {
   document.getElementById('xash-status').innerHTML = text;
+  if (text === "Scripts downloaded!") document.getElementById('xash-status').style["background-color"] = "#FEFECA";
+  else if (text === "Running...") document.getElementById("xash-status").style["background-color"] = "#C3FEC3";
+  else if (text.substring(0, 11) === "Script Error") document.getElementById("xash-status").style["background-color"] = "#FE8E8E";
 }
 
 init({
